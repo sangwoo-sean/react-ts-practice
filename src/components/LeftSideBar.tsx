d4c4e9f46fbe29645c0d6e1debd31fea000e3fc2
sangwoo <tkddn3367@gmail.com>
2023-04-02 17:57:50 +0900

    left sidebar layout

--- src/components/LeftSideBar.tsx
+++ src/components/LeftSideBar.tsx
@@ -1,3 +1,37 @@
 export function LeftSideBar() {
-  return <div>hello</div>;
+  return (
+    <div id="left-side-bar">
+      <div className="logo">
+        <img src="https://via.placeholder.com/25x25" />
+      </div>
+      <div className="tabs">
+        <ul>
+          <li>
+            <img src="https://via.placeholder.com/25x25" />
+          </li>
+          <li>
+            <img src="https://via.placeholder.com/25x25" />
+          </li>
+          <li>
+            <img src="https://via.placeholder.com/25x25" />
+          </li>
+          <li>
+            <img src="https://via.placeholder.com/25x25" />
+          </li>
+          <li>
+            <img src="https://via.placeholder.com/25x25" />
+          </li>
+          <li>
+            <img src="https://via.placeholder.com/25x25" />
+          </li>
+          <li>
+            <img src="https://via.placeholder.com/25x25" />
+          </li>
+        </ul>
+      </div>
+      <div className="menu">
+        <img src="https://via.placeholder.com/25x25" />
+      </div>
+    </div>
+  );
 }
